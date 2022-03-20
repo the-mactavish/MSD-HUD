a5e95c7c496a0857619cd43230dc0b048b74ce3d
Ayden Mactavish <inbox@macnco.one>
2022-03-20 16:23:05 +0300

    Added more stuff
    Screen Scale
    Only Icons bar
    Hot to section
    Blur and etc

--- lua/msd/ui/msd_hud.lua
+++ lua/msd/ui/msd_hud.lua
@@ -2,11 +2,38 @@ MSD.HUD = MSD.HUD or {}
 
 local HUD = MSD.HUD
 
-HUD.IconSize = 32
+HUD.IconSize = ScreenScale(12) -- Scales hud to your screen. You can set a fixed value if you want (not recomended), just replace the ScreenScale(12) with 32 and it will set icons to a fixed value.
+HUD.FondSize = math.Clamp(math.Round(HUD.IconSize / 4 + 10), 12, 52) -- Fond size for MSD can varry from 12 to 52.
+
 HUD.BarSize = 0
 HUD.WepBarSize = 0
 HUD.AnimatedBars = {}
 HUD.TextBars = {}
+
+--[[---------- HOW TO ADD CUSTOM BARS ------------
+	Animated Bars - add new item to the HUD.AnimatedBars table
+
+	HUD.AnimatedBars[ CHANGE_ID ] = {  -- IDs must go from 1 to above. ID number will effect order of bars. Lower numbers come first
+		check = function() return true end, -- add it only if you need to check for something, if false or nil is returned by this function will hide the bar element
+		icon_bg = Material("path/to/your/icon.png", "smooth"), -- Background icon, you will see it if % is lower than 100
+		icon = Material("path/to/your/icon.png", "smooth"), -- Main icon
+		max_icon = Material("path/to/your/icon.png", "smooth"), -- This icon will be shown if % is higher that 100
+		no_icon = Material("path/to/your/icon.png", "smooth"), -- This icon will be shown if % is 0 or less. If set to nil or removed, will hide the bar element if value is 0
+		smooth = 0, -- Add to smooth the transaction from 0 to 100, If set to false, nil or removed, will be ignored
+		value = function() -- value function, must return a value from 0 to 1 (representing %)
+			return LocalPlayer():Health() / LocalPlayer():GetMaxHealth() -- example, current player health divided to maximum player health
+		end
+	}
+
+	Text Bars - add new item to the HUD.TextBars table
+	HUD.TextBars[ CHANGE_ID ] = { -- IDs must go from 1 to above. ID number will effect order of bars. Lower numbers come first
+		check = function() return true end, -- add it only if you need to check for something, if false or nil is returned by this function will hide the bar element
+		icon = Material("path/to/your/icon.png", "smooth"), -- Icon material
+		text = function() -- if text set to false, nil or removed will only show the icon
+			return "My text goes here :)" -- Retur a sting of  your custom text
+		end
+	}
+]]------------------------------------------------
 
 HUD.AnimatedBars[1] = {
 	icon_bg = MSD.Icons48.heart_outline,
@@ -52,12 +79,51 @@ HUD.TextBars[2] = {
 	end
 }
 
+HUD.TextBars[3] = {
+	check = function()
+		if DarkRP and LocalPlayer():getDarkRPVar("HasGunlicense") then
+			return true
+		end
+		return false
+	end,
+	icon = MSD.Icons48.file_document,
+}
+
+local blur = Material("pp/blurscreen")
+
+function HUD.Blur(x, y, w, h)
+	surface.SetDrawColor(255, 255, 255)
+	surface.SetMaterial(blur)
+	for i = 1, 3 do
+		blur:SetFloat("$blur", (i / 4) * 4)
+		blur:Recompute()
+		render.UpdateScreenEffectTexture()
+		render.SetScissorRect(x, y, x + w, y + h, true)
+		surface.DrawTexturedRect(0, 0, ScrW(), ScrH())
+		render.SetScissorRect(0, 0, 0, 0, false)
+	end
+end
+
+function HUD.DrawBG(x, y, w, h, alpha)
+	alpha = alpha or 1
+	if MSD.Config.Blur then
+		HUD.Blur(x, y, w, h)
+		draw.RoundedBox(MSD.Config.Rounded, x, y, w, h, MSD.ColorAlpha(color_black, (250 - MSD.Config.BgrColor.r) * alpha))
+	else
+		local cl = MSD.Config.BgrColor
+		if MSD.Config.BgrColor.r > 70 then
+			cl = MSD.ColorAlpha(color_black, 255 - MSD.Config.BgrColor.r)
+		end
+		draw.RoundedBox(MSD.Config.Rounded, x, y, w, h, MSD.ColorAlpha(cl, cl.a * alpha))
+	end
+end
+
 function HUD.DrawBar(x, y)
 
 	local b = math.max(MSD.Config.Rounded, 5) + 10
 	local iy = y + 5
 
-	draw.RoundedBox(MSD.Config.Rounded, x, y, HUD.BarSize, HUD.IconSize + 10, MSD.Theme["d"])
+	HUD.DrawBG(x, y, HUD.BarSize, HUD.IconSize + 10)
 	draw.RoundedBoxEx(MSD.Config.Rounded, x, y, math.max(MSD.Config.Rounded, 5), HUD.IconSize + 10, MSD.Config.MainColor["p"], true, false, true, false)
 
 	for id, e in pairs(HUD.AnimatedBars) do
@@ -86,7 +152,7 @@ function HUD.DrawBar(x, y)
 
 		if e.no_icon or value > 0 then
 			b = b + HUD.IconSize + 5
-			b = b + 5 + math.max( draw.SimpleText( math.max(math.Round(value * 100), 0) .. "%", "MSDFont.22", x + b, y + 5 + HUD.IconSize / 2, MSD.Text["l"], 0, 1), 45)
+			b = b + 5 + math.max( draw.SimpleText( math.max(math.Round(value * 100), 0) .. "%", "MSDFont." .. HUD.FondSize, x + b, y + 5 + HUD.IconSize / 2, MSD.Text["l"], 0, 1), 45)
 		end
 	end
 
@@ -95,21 +161,10 @@ function HUD.DrawBar(x, y)
 
 		MSD.DrawTexturedRect(x + b, iy, HUD.IconSize, HUD.IconSize, e.icon, color_white)
 		b = b + HUD.IconSize + 5
-		b = b + 5 + draw.SimpleText( e.text(), "MSDFont.22", x + b, y + 5 + HUD.IconSize / 2, MSD.Text["l"], 0, 1)
+		if e.text then
+			b = b + 5 + draw.SimpleText( e.text(), "MSDFont." .. HUD.FondSize, x + b, y + 5 + HUD.IconSize / 2, MSD.Text["l"], 0, 1)
+		end
 	end
-	-- if DarkRP then
-
-	-- 	MSD.DrawTexturedRect(x + b, iy, HUD.IconSize, HUD.IconSize, MSD.Icons48.briefcase, MSD.Text["d"])
-	-- 	b = b + HUD.IconSize + 5
-	-- 	b = b + 5 + draw.SimpleText( LocalPlayer():getDarkRPVar("job") or "", "MSDFont.22", x + b, y + 5 + HUD.IconSize / 2, MSD.Text["l"], 0, 1)
-
-	-- 	local money = LocalPlayer():getDarkRPVar("money") or 0
-	-- 	MSD.DrawTexturedRect(x + b, iy, HUD.IconSize, HUD.IconSize, MSD.Icons48.cash, MSD.Text["d"])
-	-- 	b = b + HUD.IconSize + 5
-	-- 	b = b + 5 + draw.SimpleText( string.Comma(money) .. " $", "MSDFont.22", x + b, y + 5 + HUD.IconSize / 2, MSD.Text["l"], 0, 1)
-
-	-- end
-
 
 	HUD.BarSize = b + 10
 end
@@ -128,14 +183,14 @@ function HUD.DrawWeaponBar(x, y)
 	local b = rb + 10
 	local iy = y + 5
 
-	draw.RoundedBox(MSD.Config.Rounded, x - HUD.WepBarSize, y, HUD.WepBarSize, HUD.IconSize + 10, MSD.Theme["d"])
+	HUD.DrawBG(x - HUD.WepBarSize, y, HUD.WepBarSize, HUD.IconSize + 10)
 	draw.RoundedBoxEx(MSD.Config.Rounded, x - rb , y, rb, HUD.IconSize + 10, MSD.Config.MainColor["p"], false, true, false, true)
 
-	b = b + math.max( draw.SimpleText( maxammo, "MSDFont.22", x - b, y + 5 + HUD.IconSize / 2, color_white, TEXT_ALIGN_RIGHT, 1 ), 35)
+	b = b + math.max( draw.SimpleText( maxammo, "MSDFont." .. HUD.FondSize, x - b, y + 5 + HUD.IconSize / 2, color_white, TEXT_ALIGN_RIGHT, 1 ), 35)
 	b = b + HUD.IconSize
 	MSD.DrawTexturedRect(x - b, iy, HUD.IconSize, HUD.IconSize, MSD.Icons48.ammo, MSD.Text["d"])
 
-	b = b + 10 + math.max( draw.SimpleText( clip, "MSDFont.32", x - b - 10, y + 5 + HUD.IconSize / 2, color_white, TEXT_ALIGN_RIGHT, 1 ), 40)
+	b = b + 10 + math.max( draw.SimpleText( clip, "MSDFont." .. math.Clamp(HUD.FondSize + 10, 12, 52), x - b - 10, y + 5 + HUD.IconSize / 2, color_white, TEXT_ALIGN_RIGHT, 1 ), 40)
 	b = b + HUD.IconSize
 	MSD.DrawTexturedRect(x - b, iy, HUD.IconSize, HUD.IconSize, MSD.Icons48.magazine, MSD.Text["d"])
 
@@ -209,8 +264,8 @@ function HUD.DrawNotifications()
 	for k, v in ipairs(notifications) do
 		local is = math.max(MSD.Config.Rounded, 5)
 		local n_locor = note_style[v.type] and note_style[v.type].color or color_white
-		draw.RoundedBox(MSD.Config.Rounded, v.x - (v.w - (v.progress * v.w)), v.y - (k - 1) * (v.h + 5), v.w, v.h, MSD.ColorAlpha(MSD.Theme["d"], 155 * v.progress))
-		v.w = draw.SimpleText( v.text, "MSDFont.22", v.x - (v.w - (v.progress * v.w)) + 20 + HUD.IconSize, (v.y - (k - 1) * (v.h + 5)) + HUD.IconSize / 2 + 5, color_white, TEXT_ALIGN_LEFT, 1 ) + 25 + HUD.IconSize
+		HUD.DrawBG(v.x - (v.w - (v.progress * v.w)), v.y - (k - 1) * (v.h + 5), v.w, v.h, v.progress)
+		v.w = draw.SimpleText( v.text, "MSDFont." .. HUD.FondSize, v.x - (v.w - (v.progress * v.w)) + 20 + HUD.IconSize, (v.y - (k - 1) * (v.h + 5)) + HUD.IconSize / 2 + 5, color_white, TEXT_ALIGN_LEFT, 1 ) + 25 + HUD.IconSize
 
 		if note_style[v.type] then
 			if v.type == "loading" then
@@ -241,8 +296,8 @@ function HUD.DrawNotifications()
 		local is = math.max(MSD.Config.Rounded, 5)
 		local n_locor = note_style[v.type] and note_style[v.type].color or color_white
 
-		draw.RoundedBox(MSD.Config.Rounded, v.x - (v.progress * v.w) - 10, v.y - (k - 1) * (v.h + 5), v.w, v.h, MSD.ColorAlpha(MSD.Theme["d"], 155 * v.progress))
-		v.w = draw.SimpleText( v.text, "MSDFont.22", v.x - (v.progress * v.w) + 10 + HUD.IconSize - is, (v.y - (k - 1) * (v.h + 5)) + HUD.IconSize / 2 + 5, color_white, TEXT_ALIGN_LEFT, 1 ) + 25 + HUD.IconSize
+		HUD.DrawBG(v.x - (v.progress * v.w) - 10, v.y - (k - 1) * (v.h + 5), v.w, v.h, v.progress)
+		v.w = draw.SimpleText( v.text, "MSDFont." .. HUD.FondSize, v.x - (v.progress * v.w) + 10 + HUD.IconSize - is, (v.y - (k - 1) * (v.h + 5)) + HUD.IconSize / 2 + 5, color_white, TEXT_ALIGN_LEFT, 1 ) + 25 + HUD.IconSize
 
 		if note_style[v.type] then
 			MSD.DrawTexturedRect(v.x - (v.progress * v.w) + 5 - is, (v.y - (k - 1) * (v.h + 5)) + 5, HUD.IconSize, HUD.IconSize, note_style[v.type].icon, note_style[v.type].color)
